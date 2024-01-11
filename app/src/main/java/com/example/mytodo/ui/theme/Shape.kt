40a19790e31092241803cf45d00c05d41a0cddc0
Dharshib <Dharshib.8@gmail.com>
2024-01-11 18:28:32 +0530

    add theme

---
+++ app/src/main/java/com/example/mytodo/ui/theme/Shape.kt
@@ -0,0 +1,13 @@
+package com.example.mytodo.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+import androidx.compose.ui.unit.dp
+
+val Shapes = Shapes(
+
+    extraSmall = RoundedCornerShape(5.dp),
+    small = RoundedCornerShape(8.dp),
+    medium = RoundedCornerShape(16.dp),
+    large = RoundedCornerShape(25.dp)
+)
